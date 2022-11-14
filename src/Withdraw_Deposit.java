8a5785dd97d81a1ada133f80a50a65d62130185f
BigSmoke <bigsmoke@lenovo.com>
2022-11-14 20:01:43 +0530

    minor

--- src/Withdraw_Deposit.java
+++ src/Withdraw_Deposit.java
@@ -1,8 +1,6 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 
 public class Withdraw_Deposit extends Account implements Date_Time {
